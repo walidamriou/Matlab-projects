1f07b30feb9950fda2fa2dd56b7b5d26b6ff4fe6
Walid Amriou <walidamriou@gmail.com>
2019-01-05 01:03:45 +0200

    Create Contour-Detection-by-the-Gradient.m

---
+++ Digital-Image-Processing/Contour-Detection-by-the-Gradient.m
@@ -0,0 +1,16 @@
+close all; 
+clear; 
+clc; 
+I = imread('file:///C:/Users/Dinosaur/b.jpeg');
+I2=rgb2gray(I);
+c1=edge(I2,'roberts');
+c2=edge(I2,'sobel');
+c3=edge(I2,'prewitt');
+c4=edge(I2,'canny',0.6);
+
+figure(1),
+subplot(231);imshow(I);title('original');
+subplot(232);imshow(c1);title('roberts');
+subplot(233);imshow(c2);title('sobel');
+subplot(234);imshow(c3);title('prewitt');
+subplot(235);imshow(c4);title('canny');
