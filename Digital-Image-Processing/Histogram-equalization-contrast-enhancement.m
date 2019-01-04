856dbe217d045d4d35ecf0a4ff36d1503c9f8eb1
Walid Amriou <walidamriou@gmail.com>
2019-01-04 23:16:30 +0200

    Update Histogram-equalization-contrast-enhancement.m

--- Digital-Image-Processing/Histogram-equalization-contrast-enhancement.m
+++ Digital-Image-Processing/Histogram-equalization-contrast-enhancement.m
@@ -1,32 +1,22 @@
 clc;
 clear all;
-close all;
- 
+close all; 
 im=imread('file:///C:/Users/walidamriou/a.jpg');
- 
 X=rgb2gray(im);
 [m n]=size(X);
-
 Hi=imhist(X); % 1- calcule de histogramme
-
 Nbp=m*n;%calcule de pixels de l'image
-
 Hni=Hi./Nbp; % 2- Normalisation de histogramme
-
 Ci=Hni;
-
  for i=2:256;
     Ci(i)=Ci(i-1)+Hni(i);
  end
-
 F=X; % F is the new image and X is the image in gray
-
 for i=1:m
     for j=1:n
         F(i,j)=Ci(X(i,j)+1)*255;%we add  1 to start with Ci(1) because we dont had Ci(0)
     end
 end
-
 figure
 subplot(231);imshow(im);title('Image Originals');
 subplot(232);imshow(X);title('Image niveau de gris');
